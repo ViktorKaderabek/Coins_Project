5666d4ca104a1cd21d63e85b3afba68c7630a1ad
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2022-01-24 16:03:08 +0100

    Projekt

---
+++ app/src/main/java/com/example/coins_project/Presentation/Coin_List/CoinListScreen.kt
@@ -0,0 +1,42 @@
+package com.example.coins_project.Presentation.Coin_List
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.coins_project.Presentation.Coin_List.components.CoinListItem
+import com.example.coins_project.Presentation.Screen
+import org.koin.androidx.compose.inject
+
+
+@Composable
+fun CoinListScreen(
+    navController: NavController,
+) {
+     val viewModel by inject<CoinListVIewModel>()
+
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.coinsList) { coin ->
+                CoinListItem(
+                    coin = coin,
+                    onItemClick = {
+                        navController.navigate(Screen.CoinDetailScreen.route + "/${coin.id}")
+                    }
+                )
+            }
+        }
+    }
+}
